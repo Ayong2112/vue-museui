bc2f124169796da4ebed8af37ffae5b730bedbae
liuyong <liuyong@jbinfo.cn>
2018-12-19 17:31:24 +0800

    date

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,36 +2,22 @@
 	<div>
 		<v-header header='Note'></v-header>
 		<mu-container>
-			<mu-form ref="form" :model="form" class="mu-demo-form" :label-position="labelPosition" label-width="100">
-				<mu-form-item label="年月日：" prop="endNum" :rules="endNumRules">
-					<mu-date-input v-model="value1" label="选择日期" label-float full-width></mu-date-input>
+			<mu-form :model="form" class="mu-demo-form" :label-position="labelPosition" label-width="100">
+				<mu-form-item prop="beijing" label="固定地址:" >
+					<mu-text-field v-model="form.beijing" disabled></mu-text-field>
 				</mu-form-item>
-				<mu-form-item label="时间：" prop="totalNum" :rules="totalNumRules">
-					<mu-date-input  icon="today" landscape v-model="value8" label="选择时间" type="time" label-float full-width></mu-date-input>
+				<mu-form-item prop="bjDate" label="北京时间:" >
+					<mu-date-input v-model="form.bjDate" type="dateTime" actions clock-type="24hr" @change=handleotherTime()></mu-date-input>
 				</mu-form-item>
-				<mu-form-item prop="isOrder" label="现在的时区：">
-					<mu-select v-model="form.isOrder">
-						<mu-option v-for="option,index in orderOptions" :key="option" :label="option" :value="option"></mu-option>
+				<mu-form-item prop="citySelect" label="其他国家:" >
+					<mu-select v-model="form.citySelect" @change=handleCity()>
+						<mu-option v-for="otherCity,item in otherCity" :key="item.index" :label="otherCity.city" :value="otherCity.timeRange"></mu-option>
 					</mu-select>
 				</mu-form-item>
-				<mu-form-item prop="isSole" label="需要转的时区：">
-					<mu-select v-model="form.isSole">
-						<mu-option v-for="option,index in orderOptions" :key="option" :label="option" :value="option"></mu-option>
-					</mu-select>
-				</mu-form-item>
-				<mu-form-item prop="result" label="输出结果：">
-					<mu-text-field multi-line :rows="1" :rows-max="6" v-model="result" readonly="readonly"></mu-text-field>
-				</mu-form-item>
-				<mu-form-item>
-					<mu-button color="primary" @click="submit">确定</mu-button>
-					<mu-button @click="clear">重置</mu-button>
+				<mu-form-item prop="otherCityDate" label="其他国家时间:" label-width="110" >
+					<mu-date-input v-model="form.otherCityDate" type="dateTime" actions clock-type="24hr"></mu-date-input>
 				</mu-form-item>
 			</mu-form>
-			<mu-dialog title="提示" width="360" :open.sync="openSimple">
-				您好，你输入的信息有误
-				<mu-button slot="actions" flat color="primary" @click="closeSimpleDialog">我知道了</mu-button>
-			</mu-dialog>
-
 		</mu-container>
 
 		<bottom-navigation></bottom-navigation>
@@ -49,50 +35,51 @@
 
 		data() {
 			return {
-				orderOptions: ['中国', '美国', '日本','韩国','日本'],
+				otherCity: [{
+					city: '洛杉矶--太平洋时区',
+					timeRange: 16
+				}, {
+					city: '盐湖城--山地时区',
+					timeRange: 15
+				}, {
+					city: '芝加哥--中部时区',
+					timeRange: 14
+				}, {
+					city: '纽约、华盛顿--东部时区',
+					timeRange: 13
+				}, {
+					city: '火奴鲁鲁--夏威夷时区',
+					timeRange: 18
+				}, {
+					city: '费尔班克斯--阿拉斯加时区',
+					timeRange: 17
+				}],
 				labelPosition: 'left',
-				openSimple: false,
-				result: '',
 				form: {
-					startNum: '',
-					endNum: '',
-					totalNum: '',
-					isSole: '中国',
-					isOrder: '美国',
-				},
+					beijing: '北京',
+					bjDate:"",
+					citySelect:'洛杉矶--太平洋时区',
+					otherCityDate:"",
+				}
 			}
 		},
+			// html渲染前
+				created() {
+					// 显示当前北京时间
+					this.form.bjDate = new Date()
+					// 换算出洛杉矶的时间
+					let timerange = 16 * 60 * 60 * 1000
+					let curDate = new Date().getTime()
+					var result = curDate - timerange;
+					this.form.otherCityDate = new Date(result)
+				},
 		methods: {
-			closeSimpleDialog() {
-				this.openSimple = false;
+			handleCity() {
+				console.log(this.form.citySelect)//打印的时间
 			},
-			submit() {
-				this.$refs.form.validate().then((result) => {
-					if(!result) {
-						this.openSimple = true;
-					} else {
-						this.getRandomNum(this.form)
-					}
-				});
-			},
-			clear() {
-				this.$refs.form.clear();
-				this.result = '';
-				this.form = {
-					startNum: '',
-					endNum: '',
-					totalNum: '',
-					isSole: '中国',
-					isOrder: '美国',
-				};
-			},
+			handleotherTime(){
+				console.log(this.form.bjDate)
+			}
 		}
 	}
 </script>
-<style scoped>
-	.mu-demo-form {
-		width: 100%;
-		max-width: 460px;
-	}
-	
-	</style>
