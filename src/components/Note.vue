25e53af3792cc38ccb6e5bec3c82e44563b93afd
liuyong <liuyong@jbinfo.cn>
2018-12-21 17:07:26 +0800

    date

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,22 +2,48 @@
 	<div>
 		<v-header header='Note'></v-header>
 		<mu-container>
-			<mu-form :model="form" class="mu-demo-form" :label-position="labelPosition" label-width="100">
-				<mu-form-item prop="beijing" label="固定地址:" >
-					<mu-text-field v-model="form.beijing" disabled></mu-text-field>
-				</mu-form-item>
-				<mu-form-item prop="bjDate" label="北京时间:" >
-					<mu-date-input v-model="form.bjDate" type="dateTime" actions clock-type="24hr" @change=handleotherTime()></mu-date-input>
-				</mu-form-item>
-				<mu-form-item prop="citySelect" label="其他国家:" >
-					<mu-select v-model="form.citySelect" @change=handleCity()>
+			<mu-row align-items='baseline'>
+				<mu-col span="3">
+					<div>固定地址:</div>
+				</mu-col>
+				<mu-col span="7">
+					<mu-text-field v-model="beijing" disabled></mu-text-field>
+				</mu-col>
+			</mu-row>
+			<mu-row align-items='baseline'>
+				<mu-col span="3">
+					<div>北京时间:</div>
+				</mu-col>
+				<mu-col span="7">
+					<mu-date-input v-model="bjDate" type="dateTime" actions clock-type="24hr" @change="handleOldBjDate()"></mu-date-input>
+				</mu-col>
+			</mu-row>
+			<mu-row align-items='baseline'>
+				<mu-col span="3">
+					<div>换算地区:</div>
+				</mu-col>
+				<mu-col span="7">
+					<mu-select v-model="citySelect" @change='handleChangeCity()'>
 						<mu-option v-for="otherCity,item in otherCity" :key="item.index" :label="otherCity.city" :value="otherCity.timeRange"></mu-option>
 					</mu-select>
-				</mu-form-item>
-				<mu-form-item prop="otherCityDate" label="其他国家时间:" label-width="110" >
-					<mu-date-input v-model="form.otherCityDate" type="dateTime" actions clock-type="24hr"></mu-date-input>
-				</mu-form-item>
-			</mu-form>
+				</mu-col>
+			</mu-row>
+			<mu-row align-items='baseline'>
+				<mu-col span="3">
+					<div>换算地区时间:</div>
+				</mu-col>
+				<mu-col span="7">
+					<mu-date-input v-model="otherCityDate" type="dateTime" actions clock-type="24hr" @change="hangleOldOtheDate()"></mu-date-input>
+				</mu-col>
+			</mu-row>
+			<mu-row align-items='baseline'>
+				<mu-col>
+					<div>只有更改换算地区时间才会修改北京的时间</div>
+				</mu-col>
+			</mu-row>
+
+
+
 		</mu-container>
 
 		<bottom-navigation></bottom-navigation>
@@ -54,31 +80,87 @@
 					city: '费尔班克斯--阿拉斯加时区',
 					timeRange: 17
 				}],
-				labelPosition: 'left',
-				form: {
-					beijing: '北京',
-					bjDate:"",
-					citySelect:'洛杉矶--太平洋时区',
-					otherCityDate:"",
-				}
+				beijing: '北京',
+				bjDate: "",
+				citySelect: '洛杉矶--太平洋时区',
+				otherCityDate: "",
+				oldBjDate: "",
+				oldOtherCityDate: ''
 			}
 		},
-			// html渲染前
-				created() {
-					// 显示当前北京时间
-					this.form.bjDate = new Date()
-					// 换算出洛杉矶的时间
-					let timerange = 16 * 60 * 60 * 1000
-					let curDate = new Date().getTime()
-					var result = curDate - timerange;
-					this.form.otherCityDate = new Date(result)
-				},
-		methods: {
-			handleCity() {
-				console.log(this.form.citySelect)//打印的时间
+		watch: {
+			bjDate: function() {
+				this.handleChangeDate("1")
 			},
-			handleotherTime(){
-				console.log(this.form.bjDate)
+			otherCityDate: function() {
+				this.handleChangeDate("2")
+			}
+		},
+		// html渲染前
+		created() {
+			// 显示当前北京时间
+			this.bjDate = new Date()
+			this.oldBjDate = new Date()
+			// 换算出洛杉矶的时间
+			let timerange = 16 * 60 * 60 * 1000
+			let curDate = new Date().getTime()
+			var result = curDate - timerange;
+			this.otherCityDate = new Date(result)
+			this.oldOtherCityDate = new Date(result)
+		},
+		methods: {
+
+			handleOldBjDate() {
+				this.oldBjDate = this.bjDate
+			},
+			hangleOldOtheDate() {
+				this.oldOtherCityDate = this.otherCityDate
+			},
+			handleChangeCity() {
+				if (this.citySelect == "洛杉矶--太平洋时区") {
+					this.otherCityDate = new Date(new Date(this.bjDate) - (16 * 60 * 60 * 1000))
+				} else {
+					this.otherCityDate = new Date(new Date(this.bjDate) - (+this.citySelect * 60 * 60 * 1000))
+				}
+			},
+			handleChangeDate(value) {
+				let tt = {
+					nj: this.bjDate,
+					oj: this.oldBjDate,
+					no: this.otherCityDate,
+					oo: this.oldOtherCityDate,
+					njt: new Date(this.bjDate).getTime(),
+					ojt: new Date(this.oldBjDate).getTime(),
+					not: new Date(this.otherCityDate).getTime(),
+					oot: new Date(this.oldOtherCityDate).getTime(),
+				}
+
+				console.log(value)
+				if (value == "1") {
+					if (tt.njt == tt.ojt) {
+						debugger
+					} else {
+						debugger
+						if (this.citySelect == "洛杉矶--太平洋时区") {
+							this.otherCityDate = new Date(new Date(this.bjDate) - (16 * 60 * 60 * 1000))
+						} else {
+							this.otherCityDate = new Date(new Date(this.bjDate) - (+this.citySelect * 60 * 60 * 1000))
+						}
+					}
+
+				} else if (value == "2") {
+					if (tt.not == tt.oot) {
+						debugger
+					} else {
+						debugger
+						if (this.citySelect == "洛杉矶--太平洋时区") {
+							this.bjDate = new Date(new Date(this.otherCityDate) + (16 * 60 * 60 * 1000))
+						} else {
+							this.bjDate = new Date(new Date(this.otherCityDate) + (+this.citySelect * 60 * 60 * 1000))
+						}
+					}
+
+				}
 			}
 		}
 	}
